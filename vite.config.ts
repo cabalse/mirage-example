9af1c030becf993852d62fc35eb864a014c2ee6d
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-23 10:04:49 +0200

    Add Rollup config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../dist',
+    assetsDir: '.',
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/[name]-[hash].js',
+        chunkFileNames: 'js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const extType = info[info.length - 1]
+          if (/\.(png|jpe?g|gif|svg|webp|webm|mp3)$/.test(assetInfo.name)) {
+            return `media/[name]-[hash].${extType}`
+          }
+          if (/\.(css)$/.test(assetInfo.name)) {
+            return `css/[name]-[hash].${extType}`
+          }
+          if (/\.(woff|woff2|eot|ttf|otf)$/.test(assetInfo.name)) {
+            return `fonts/[name]-[hash].${extType}`
+          }
+          return `[name]-[hash].${extType}`
+        },
+      },
+    },
+  },
 })
