fb26440e5786244b4716eaa605fb8fc67fc52d98
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-23 10:33:25 +0200

    Add api-mock-server

---
+++ api-mock-server/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("API Mock Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`API Mock server listening on port ${port}`);
+});
