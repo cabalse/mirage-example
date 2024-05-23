938afab2a98f060565ff2b69e8e3e47d1d36d71d
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-23 14:06:23 +0200

    Add json-server

--- api-mock-server/server.js
+++ api-mock-server/server.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const cors = require("cors");
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(cors());
+app.use(cors())
 
-app.get("/", (req, res) => {
-  res.send("API Mock Server is running");
-});
+app.get('/', (req, res) => {
+  res.send('API Mock Server is running')
+})
 
 app.listen(port, () => {
-  console.log(`API Mock server listening on port ${port}`);
-});
+  console.log(`API Mock server listening on port ${port}`)
+})
