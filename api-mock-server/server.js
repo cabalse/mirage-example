85fb54d6312b9c837a9fa10c167359ef1ddbfda9
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-23 10:36:28 +0200

    Add api films call to server

--- api-mock-server/server.js
+++ api-mock-server/server.js
@@ -12,6 +12,31 @@ app.get('/', (req, res) => {
   res.send('API Mock Server is running')
 })
 
+app.get('/api/films/', (req, res) => {
+  res.send({
+    results: [
+      {
+        episode_id: 4,
+        title: 'A New Hope',
+        director: 'George Lucas',
+        release_date: '1977-05-25',
+      },
+      {
+        episode_id: 5,
+        title: 'The Empire Strikes Back',
+        director: 'Irvin Kershner',
+        release_date: '1980-05-17',
+      },
+      {
+        episode_id: 6,
+        title: 'Return of the Jedi',
+        director: 'Richard Marquand',
+        release_date: '1983-05-25',
+      },
+    ],
+  })
+})
+
 app.listen(port, () => {
   console.log(`API Mock server listening on port ${port}`)
 })
