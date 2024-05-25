ac81e90a2200ac3b72b06364a5d7b08bf22ed311
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-25 09:50:02 +0200

    3. Add route for films

--- src/mirage-server/index.js
+++ src/mirage-server/index.js
@@ -1,9 +1,16 @@
 import { createServer } from 'miragejs'
+import movies from '../data/movies'
 
 const mirageServer = () => {
   createServer({
     routes() {
       this.urlPrefix = 'https://swapi.dev/api'
+
+      this.get('/films', () => {
+        return {
+          results: movies,
+        }
+      })
     },
   })
 }
