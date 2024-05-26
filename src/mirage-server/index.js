70d69edb62078c6085f225d8f1d081ddd95f9857
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-26 18:17:29 +0200

    5. Schema, seed and post route

--- src/mirage-server/index.js
+++ src/mirage-server/index.js
@@ -1,17 +1,44 @@
-import { createServer } from 'miragejs'
-import movies from '../data/movies'
+import { Model, createServer } from 'miragejs'
 
 const mirageServer = ({ environment = 'test' }) => {
   createServer({
     environment,
 
+    models: {
+      results: Model,
+    },
+
+    seeds(server) {
+      server.schema.results.create({
+        episode_id: 4,
+        title: 'A New Hope',
+        director: 'George Lucas',
+        release_date: '1977-05-25',
+      })
+      server.schema.results.create({
+        episode_id: 5,
+        title: 'The Empire Strikes Back',
+        director: 'Irvin Kershner',
+        release_date: '1980-05-17',
+      })
+      server.schema.results.create({
+        episode_id: 6,
+        title: 'Return of the Jedi',
+        director: 'Richard Marquand',
+        release_date: '1983-05-25',
+      })
+    },
+
     routes() {
       this.urlPrefix = 'https://swapi.dev/api'
 
-      this.get('/films', () => {
-        return {
-          results: movies,
-        }
+      this.get('/films', (schema) => {
+        return schema.results.all()
+      })
+
+      this.post('/films', (schema, request) => {
+        const attrs = JSON.parse(request.requestBody)
+        schema.results.create(attrs)
       })
     },
   })
