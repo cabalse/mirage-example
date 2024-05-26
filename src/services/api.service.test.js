f5e244e67927045e12005c2b5ee798af8844f42f
Jörgen Bengtsson <jorgen.bengtsson@guardtools.com>
2024-05-26 20:48:30 +0200

    Correct test

--- src/services/api.service.test.js
+++ src/services/api.service.test.js
@@ -4,8 +4,7 @@ import { getMovies } from './api.services'
 
 describe('Api.Services', () => {
   test('Api call getMovies shall return six movies', async () => {
-    getMovies().then((response: any) => {
-      expect(response.data.results).toHaveLength(6)
-    })
+    const response = await getMovies()
+    expect(response.data.results).toHaveLength(6)
   })
 })
